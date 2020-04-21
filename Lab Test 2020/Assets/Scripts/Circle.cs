75e53c84653116b7765725f7e79f445b18874846
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-04-21 10:55:14 +0100

    Circle of lights provedurally generating

---
+++ Lab Test 2020/Assets/Scripts/Circle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public GameObject tLightPrefab;
+    [SerializeField] int amountOfLights = 10;
+    [SerializeField] private GameObject[] traficLights;
+    
+
+    void Start()
+    {
+        traficLights = new GameObject[amountOfLights];
+        for(int i = 0; i < amountOfLights; i ++)
+        {
+            GameObject tLightInstance = (GameObject)Instantiate(tLightPrefab);
+            tLightInstance.transform.position = this.transform.position;
+            tLightInstance.transform.parent = this.transform; //make the trafic lights children of this gameobject
+            tLightInstance.name = "Trafic Light " + (i + 1);
+            this.transform.eulerAngles = new Vector3(0, (360 / amountOfLights * -1) * i, 0);
+            tLightInstance.transform.position = Vector3.forward * 10;
+            traficLights[i] = tLightInstance;
+        }
+    }
+
+    
+    void Update()
+    {
+
+    }
+}
